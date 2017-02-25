f8b6775152d055b05814c12b4115b38890349134
unknown <alvaro.izquierdog@SALAF402-27.UAM.LOCAL>
2017-02-25 09:38:29 -0500

    Se crearon vistas para el controlador del sitio

--- app/Http/Controllers/SitioController.php
+++ app/Http/Controllers/SitioController.php
@@ -7,10 +7,13 @@ use Illuminate\Http\Request;
 class SitioController extends Controller
 {
     public function index(){
-        echo "Hola Mundo";
+        //echo "Hola Mundo";
+        return view('sitio/index');
     }
 
     public function saludar(Request $request, $nombre){
-        echo "Hola " . $nombre;
+        //echo "Hola " . $nombre;
+         return view('sitio/saludar',
+                    ['elNombre'=>$nombre]);
     }
 }
---
+++ resources/views/sitio/saludar.blade.php
@@ -0,0 +1,1 @@
+Hola {!! $elNombre !!} saludos desde una vista
